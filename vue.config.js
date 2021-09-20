5c2204d72f5df06c521b8442736bd5e7648c0088
porcelanosa <public_mce@rambler.ru>
2021-09-20 14:31:47 +0300

    Added PUG and SASS

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  chainWebpack: (config) => {
+    // Pug Loader
+    config.module
+      .rule('pug')
+      .test(/\.pug$/)
+      .use('pug-plain-loader')
+      .loader('pug-plain-loader')
+      .end();
+  },
+}
