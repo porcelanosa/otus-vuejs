38b2c01e4a00c961bfab95b838e3fe0c759f3bdd
porcelanosa <public_mce@rambler.ru>
2021-09-21 13:18:38 +0300

    Import sass files

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import('./assets/css/main.sass')
 
 export default {
   name: 'App',
@@ -16,11 +17,4 @@ export default {
 </script>
 
 <style lang="sass">
-#app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
 </style>
